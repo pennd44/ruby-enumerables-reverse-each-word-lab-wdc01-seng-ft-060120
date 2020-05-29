5f0e46128314524408cee3564822bcf27b9ae186
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 18:38:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   array_of_words = string.split(/ /)
-  array_of_words.collect do |element|
+  new_array = array_of_words.collect do |element|
     element.reverse
   end
-  array_of_words.join(" ")
+  new_array.join(" ")
 end
